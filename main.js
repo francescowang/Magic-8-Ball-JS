21207dec88bd213651bd18017955db124edb1cf1
francesco wang <francescowang@outlook.com>
2022-03-29 11:20:33 +0100

    firesound sound

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 function yourQuestion() {
     let answerOption = ["yes", "no", "let me think about it", "maybe", "probably yes", "probably not", "ask again", "it's a stupid question if I'm honest"];
     let result = answerOption[Math.floor(Math.random() * answerOption.length)];
-    // var audio = new Audio('data/firework.mp3');
+    var audio = new Audio('data/firework_sound.mp3');
     let userAnswer = document.getElementById("QUESTION").value
     // document.getElementById("magic8ballquestion").innerHTML = "Your question: " + user_answer;
     document.getElementById("magic8ballanswer").innerHTML = "The ball says: " + result;
