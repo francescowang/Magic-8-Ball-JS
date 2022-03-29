c421d9e43efdff209b21a7bdb18bfd45fccc3de8
AshaF4 <asha.fidow@kpmg.co.uk>
2022-03-29 11:02:54 +0100

    changes to css

--- main.js
+++ main.js
@@ -1,8 +1,8 @@
 function yourQuestion() {
-    var answer_option = ["yes", "no", "let me think about it", "maybe", "probably yes", "probably not", "ask again", "it's a stupid question if I'm honest"];
-    var result = answer_option[Math.floor(Math.random() * answer_option.length)];
+    let answerOption = ["yes", "no", "let me think about it", "maybe", "probably yes", "probably not", "ask again", "it's a stupid question if I'm honest"];
+    let result = answerOption[Math.floor(Math.random() * answerOption.length)];
     // var audio = new Audio('data/firework.mp3');
-    var user_answer = document.getElementById("QUESTION").value
+    let userAnswer = document.getElementById("QUESTION").value
     // document.getElementById("magic8ballquestion").innerHTML = "Your question: " + user_answer;
     document.getElementById("magic8ballanswer").innerHTML = "The ball says: " + result;
     document.getElementById("QUESTION").value="";
