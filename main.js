d05ccb31c25db2607e2a8d2638759b3cc67654c1
AshaF4 <asha.fidow@kpmg.co.uk>
2022-03-30 19:15:43 +0100

    added module for answers

--- main.js
+++ main.js
@@ -1,6 +1,10 @@
+import {answers} from "./modules/answers.js"
+
+let submitButton = document.querySelector("#fortunebutton");
+let resetTheQuestion = document.querySelector("#resetbutton");
+
 function yourQuestion() {
-    let answerOption = ["yes", "no", "let me think about it", "maybe", "probably yes", "probably not", "ask again", "it's a stupid question if I'm honest"];
-    let result = answerOption[Math.floor(Math.random() * answerOption.length)];
+    let result = answers[Math.floor(Math.random() * answers.length)];
     let audio = new Audio('data/firework_sound.mp3');
     document.getElementById("magic8ballanswer").innerHTML = "The ball says: " + result;
     document.getElementById('answer').style.display = "inline";
@@ -12,3 +16,6 @@ function yourQuestion() {
 function resetQuestion(){
     window.location.reload();
 };
+
+submitButton.addEventListener('click', yourQuestion);
+resetTheQuestion.addEventListener('click', resetQuestion);
