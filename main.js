6a7c69070cd4e1a23786f50ac176e9ab457a7670
Yallur <prajwalyallur@kpmg.com>
2022-03-31 19:02:58 +0530

    answer after sound with delay and ask next question

--- main.js
+++ main.js
@@ -7,10 +7,14 @@ function yourQuestion() {
     let result = answers[Math.floor(Math.random() * answers.length)];
     let audio = new Audio('data/firework_sound.mp3');
     document.getElementById("magic8ballanswer").innerHTML = "The ball says: " + result;
-    document.getElementById('answer').style.display = "inline";
-    document.getElementById('resetbutton').style.display = "inline";
-    document.getElementById("question").value="";
     audio.play();
+    setTimeout(function() {
+        document.getElementById('answer').style.display = "inline";
+        document.getElementById('resetbutton').style.display = "inline";
+        document.getElementById("question").value="";
+        }, 1000);
+    
+    
 };
 
 function resetQuestion(){
