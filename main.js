19fe9cd3925700ed5397fa538f26da374ee702e1
Yallur <prajwalyallur@kpmg.com>
2022-03-31 20:32:15 +0530

    answer after sound with delay and ask next question

--- main.js
+++ main.js
@@ -5,21 +5,24 @@ let resetTheQuestion = document.querySelector("#resetbutton");
 
 function yourQuestion() {
     let result = answers[Math.floor(Math.random() * answers.length)];
-    let audio = new Audio('data/firework_sound.mp3');
+    let audio1 = new Audio('data/firework_sound.mp3');
+    let audio2 = new Audio('data/Tada-sound.mp3');
     document.getElementById("magic8ballanswer").innerHTML = "The ball says: " + result;
-    audio.play();
+    audio1.play();
     setTimeout(function() {
         document.getElementById('answer').style.display = "inline";
+        audio2.play();
+        }, 1200);
+    setTimeout(function() {
         document.getElementById('resetbutton').style.display = "inline";
-        document.getElementById("question").value="";
-        }, 1000);
-    
-    
+        }, 2000);
+       
 };
 
 function resetQuestion(){
     window.location.reload();
 };
+
 
 submitButton.addEventListener('click', yourQuestion);
 resetTheQuestion.addEventListener('click', resetQuestion);
